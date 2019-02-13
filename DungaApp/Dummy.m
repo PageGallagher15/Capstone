03366528913cd642c92de94ea23b984b4b38d8fc
Brad Wallis <bwallis24@gmail.com>
2019-02-13 16:59:54 -0500

    Firebase Set Up
    Contains Everything

---
+++ DungaApp/Dummy.m
@@ -0,0 +1,11 @@
+//
+//  Dummy.m
+//  DungaApp
+//
+//  Created by Wallis Bradley W. on 2/13/19.
+//  Copyright © 2019 HotFireGames. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//#import "Firebase.h"
