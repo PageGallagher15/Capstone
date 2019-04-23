43f11de90e6c231d038fe23b32615b5c35ca55c1
Wallis Bradley W <n00434189@nl-00205-031.mediastudies.ca>
2019-04-23 16:28:58 -0400

    Initial Audio Working

---
+++ DungaApp/DungaApp/DungaAudio.swift
@@ -0,0 +1,43 @@
+//
+//  DungaAudio.swift
+//  DungaApp
+//
+//  Created by Wallis Bradley W. on 4/23/19.
+//  Copyright © 2019 HotFireGames. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class DungaAudio: UIViewController {
+    
+    var audioPlayer = AVAudioPlayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    
+    @IBAction func DungaAudio1(_ sender: Any) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path (forResource: "DungaSingingLong", ofType: "wav")!))
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+        }
+            
+        catch {
+            print(error)
+        }
+
+    }
+    
+}
+
